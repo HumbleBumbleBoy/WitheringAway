05ea50a56046b6f80d945b185f875b61544820c8
Humble Bumble <157408786+HumbleBumbleBoy@users.noreply.github.com>
2025-11-22 13:29:16 +0100

    More cards and animation folders

---
+++ Game elements/Cards/Qman/QMan.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class QMan : BaseCardTemplate
+{
+    // choose a lane to spawn a bird in (2HP/3ATK)
+}
---
+++ Game elements/Cards/VoodooDoll/VoodooDoll.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class VoodooDoll : BaseCardTemplate
+{
+    // on take damage, deal damage back to atacker
+}

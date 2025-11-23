292dda532d02d04753eafbe43651ccd2df75b905
Humble Bumble <157408786+HumbleBumbleBoy@users.noreply.github.com>
2025-11-23 01:15:55 +0100

    Added a bunch of art

--- Game elements/GameScene/GameScene.cs
+++ Game elements/GameScene/GameScene.cs
@@ -27,37 +27,6 @@ public partial class GameScene : Node2D
         playerHandManager?.GetTopCard();
     }
 
-    private void onSpawnDeckButtonPressed()
-    {
-        int randomDeckIndex = new Random().Next(0, playerDeckManager?.Decks.Length??0);
-        PackedScene[] selectedDeckCards = playerDeckManager?.Decks[randomDeckIndex].Cards??[];
-        
-        foreach (PackedScene packedScene in selectedDeckCards)
-        {
-            playerDeckManager?.addCardToDeck(packedScene);
-            playerDeckManager?.addCardToDeckVisually(packedScene);
-        }
-    }
-
-    private void onKillCardOnFieldButton()
-    {
-        FieldData fieldData = GetNode<FieldData>("/root/GameScene/FieldData");
-    
-        // Get all non-null cards on field
-        var aliveCards = fieldData.PlayerCardsOnField.Where(card => card != null).ToList();
-        
-        if (aliveCards.Count > 0)
-        {
-            // Pick a random card and kill it
-            var randomCard = aliveCards[new Random().Next(0, aliveCards.Count)];
-            randomCard?.StateMachine.ChangeState(new CardDied(true));
-            GD.Print("Killed: " + randomCard?.Name);
-        }
-        else
-        {
-            GD.Print("No cards on field to kill");
-        }
-    }
 
     private void OnPassTurnButtonPressed()
     {
